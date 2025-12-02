87fb564a858a22c8d0f6ee5d42547880df7dee46
theme-memo <245191659+theme-memo@users.noreply.github.com>
2025-12-02 16:39:33 +0000

    refactor: fix params error

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,38 @@
 export default {
-	async scheduled(env: Env) {
-		const lat = 43.33003301715253;
-		const lon = 145.5976092292395;
-		const apiKey = env.OPENWEATHERMAP_APIKEY.get();
+	async scheduled(controller, env, ctx) {
+		ctx.waitUntil(UpdateWeather(env));
+	},
+};
 
-		if (!apiKey) {
-			console.error('OPENWEATHERMAP_APIKEY secret not configured');
+async function UpdateWeather(env) {
+	const lat = 43.33003301715253;
+	const lon = 145.5976092292395;
+	const apiKey = env.OPENWEATHERMAP_APIKEY.get();
+
+	if (!apiKey) {
+		console.error('OPENWEATHERMAP_APIKEY secret not configured');
+		return;
+	}
+
+	const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}&units=metric&lang=ja`;
+
+	try {
+		const weatherResponse = await fetch(weatherUrl);
+		if (!weatherResponse.ok) {
+			const errorText = await weatherResponse.text();
+			console.error('Failed to fetch weather data', errorText);
 			return;
 		}
 
-		const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${apiKey}&units=metric&lang=ja`;
+		const weatherData = await weatherResponse.json();
 
-		try {
-			const weatherResponse = await fetch(weatherUrl);
-			if (!weatherResponse.ok) {
-				const errorText = await weatherResponse.text();
-				console.error('Failed to fetch weather data', errorText);
-				return;
-			}
-
-			const weatherData = await weatherResponse.json();
-
-			await env.R2_BUCKET.put('weather.json', JSON.stringify(weatherData));
-			console.log('Successfully fetched and stored weather data.');
-		} catch (e) {
-			if (e instanceof Error) {
-				console.error('Failed to fetch weather data', e.message);
-			} else {
-				console.error('Failed to fetch weather data', 'Unknown error');
-			}
+		await env.R2_BUCKET.put('weather.json', JSON.stringify(weatherData));
+		console.log('Successfully fetched and stored weather data.');
+	} catch (e) {
+		if (e instanceof Error) {
+			console.error('Failed to fetch weather data', e.message);
+		} else {
+			console.error('Failed to fetch weather data', 'Unknown error');
 		}
-	},
-};
+	}
+}
